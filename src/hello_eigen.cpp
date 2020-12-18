8d578759e361d397fc7fff0ae9fa7d248d26215f
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2020-12-19 00:03:30 +0900

    add eigen3 example code

---
+++ src/hello_eigen.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
+
+int main()
+{
+    MatrixXd a(2, 2), b(2, 2);
+    a << 1, 2, 3, 4;
+    b << 5, 4, 3, 2;
+
+    std::cout << "a=\n" << a << std::endl;
+    std::cout << "b=\n" << b << std::endl;
+    // add
+    std::cout << "a+b=\n" << a + b << std::endl;
+    // sub
+    std::cout << "a-b=\n" << a - b << std::endl;
+    // mul
+    std::cout << "a*b=\n" << a * b << std::endl;
+}
